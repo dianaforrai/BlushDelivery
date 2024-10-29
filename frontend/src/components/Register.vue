62a3f5adb79772cdb5d2d735e59d2ad39f9468ab
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-10-29 12:35:24 +0200

    register page

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container-fluid login-container">
+      <form class="main-form">
+        <h1 class="text-center">Register</h1>
+        <div class="mb-3">
+          <label for="inputUsername" class="form-label">Username</label>
+          <input v-model="username" type="text" class="form-control" id="inputUsername" required>
+        </div>
+        <div class="mb-3">
+          <label for="inputRepeatPassword" class="form-label">Password</label>
+          <input v-model="repeatPassword" type="password" class="form-control" id="inputRepeatPassword"
+            aria-describedby="pwHelp" required>
+        </div>
+        <div class="mb-3">
+          <label for="inputPassword" class="form-label">Repeat password</label>
+          <input v-model="password" type="password" class="form-control" id="inputPassword" aria-describedby="pwHelp"
+            required>
+        </div>
+        <div id="pwHelp" class="form-text">We'll never share your password with anyone else.</div>
+        <button @click="register($event)" type="submit" class="btn btn-primary">Register</button>
+      </form>
+  
+  
+  
+  
+      <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-autohide="true"
+        :class="{ 'show': showToast }" style="position: absolute; top: 0; right: 0;">
+        <div class="toast-header">
+          <strong class="me-auto">Notification</strong>
+          <button type="button" class="m1-2 mb-1 btn-close" @click="showToast = false"></button>
+        </div>
+        <div class="toast-body">
+          {{ toastMessage }}
+        </div>
+      </div>
+  
+    </div>
+  </template>
+  <script>
+
+export default {
+  name: 'UserRegistration',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+      repeatPassword: '',
+      showToast: false,
+      toastMessage: ''
+    };
+  },
+  methods: {
+    register(event) {
+        event.preventDefault();
+    },  
+}
+}
+
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,13 +4,17 @@ import ViewPackages from '../components/ViewPackages.vue'
 import AvailablePackages from '../components/AvailablePackages.vue'
 import AddPackages from '../components/AddPackages.vue'
 import DeliverPackages from '../components/DeliverPackages.vue'
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
 
 const routes = [
   { path : '/', component : Home },
   { path : '/view-packages', component : ViewPackages },
   { path : '/available-packages', component : AvailablePackages },
   { path : '/add-package', component : AddPackages },
-  { path : '/deliver-package', component : DeliverPackages }
+  { path : '/deliver-package', component : DeliverPackages },
+  { path : '/login', component : Login },
+  { path : '/register', component : Register }
 ];
 
 const router = createRouter({

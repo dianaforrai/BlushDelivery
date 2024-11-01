bb93903fb84ecac4d5f13fc4fd02a51210354e03
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-11-01 21:18:10 +0200

    dev

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,13 +3,13 @@
     <nav v-if="$route.path !== '/login' && $route.path !== '/register'">
     <ul class = "lista">
       <li class="main-navigation"><RouterLink to="/"><span class="main-navigation">Home</span></RouterLink></li>
+      <li class="main-navigation"> <RouterLink to="/add-package"><span class="main-navigation">Add Package</span></RouterLink></li> 
+      <li class="main-navigation"><RouterLink to="/delete-packages"><span class="main-navigation">Delete Package</span></RouterLink></li>
       <li class="main-navigation"><RouterLink to="/view-packages"><span class="main-navigation">View Packages</span></RouterLink></li>
       <li class="main-navigation"><RouterLink to="/available-packages"><span class="main-navigation">Available Packages</span></RouterLink></li>     
-      <li class="main-navigation"> <RouterLink to="/add-package"><span class="main-navigation">Add Package</span></RouterLink></li>    
       <li class="main-navigation"><RouterLink to="/deliver-package"><span class="main-navigation">Deliver Package</span></RouterLink></li>
       <li class="main-navigation"><RouterLink to="/assign-package"><span class="main-navigation">Assign Package</span></RouterLink></li>
       <li class="main-navigation"><RouterLink to="/set-manager"><span class="main-navigation">Set Manager</span></RouterLink></li>
-      <li class="main-navigation"><RouterLink to="/delete-packages"><span class="main-navigation">Delete Package</span></RouterLink></li>
     </ul>
     <button @click="logout()" class="btn-logout"> <i class="fa-solid fa-right-from-bracket"></i> Logout</button>
     </nav>
